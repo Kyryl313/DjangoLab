7c34f5255bb7e8a8f279766213208530818754c9
Kyryl313 <flash73627@gmail.com>
2026-04-17 09:51:50 +0300

    Lab4

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Dish, Order
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'created_at', 'updated_at')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('dish', 'quantity', 'created_at', 'updated_at')
+
+
+from django.contrib import admin
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Замовлення: {self.dish.name} x{self.quantity}"
